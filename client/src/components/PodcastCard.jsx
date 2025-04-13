6ee415884c0d61cf04ffa2cae7fd1f909408b4be
Rajvardhan <I2K221241@ms.pict.edu>
2025-04-14 00:32:07 +0530

    Test21 Commit

--- client/src/components/PodcastCard.jsx
+++ client/src/components/PodcastCard.jsx
@@ -148,18 +148,25 @@ const PodcastCard = ({ podcast, onLike, canPlay }) => {
             }}
           />
           <div className="absolute inset-0 bg-black bg-opacity-40 opacity-0 group-hover:opacity-100 transition-opacity duration-300 flex items-center justify-center">
-            {/* {canPlay ? ( */}
+            {canPlay ? (
               <button
                 onClick={handlePlay}
                 className="bg-white text-blue-600 p-4 rounded-full transform scale-75 group-hover:scale-100 transition-transform duration-300"
               >
-                <FiPlay className="w-6 h-6" />
+                 
+                  <FiPlay className="w-6 h-6" /> 
+                
               </button>
-            {/* ) : (
-              <Link to="/register" className="bg-white text-gray-400 p-4 rounded-full transform scale-75 group-hover:scale-100 transition-transform duration-300">
-                <FiPlay className="w-6 h-6" />
-              </Link>
-            )} */}
+            ) : (
+              <button
+                onClick={handlePlay}
+                className="bg-white text-blue-600 p-4 rounded-full transform scale-75 group-hover:scale-100 transition-transform duration-300"
+              >
+                 
+                  <FiPlay className="w-6 h-6" /> 
+                
+              </button>
+            )} 
           </div>
         </div>
 
