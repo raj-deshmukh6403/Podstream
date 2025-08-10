bd2f8b4b104153b2ba27a8ec110f4cc7fad2a767
Atharva Desai <datharva919@gmail.com>
2025-08-10 20:56:37 +0530

    Update contactRoutes.js
    fixed error

--- server/routes/contactRoutes.js
+++ server/routes/contactRoutes.js
@@ -3,10 +3,12 @@ const express = require('express');
 const router = express.Router();
 const contactController = require('../controllers/contactcontroller');
 
+
 // Contact form submission route
 router.post('/contact', contactController.sendContactMessage);
 
 // Newsletter subscription route
 router.post('/subscribenews', contactController.subscribeToNewsletter);
+
 
 module.exports = router;
